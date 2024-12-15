01365f84603643a0508a46d3c07ff3c49dde2028
xavier gomez <axaviergp@gmail.com>
2024-12-14 23:40:20 -0600

    Adding heroes folder and hero, list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     // Se usa el nombre de la clase
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
