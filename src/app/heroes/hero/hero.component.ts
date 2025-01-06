0414adb7dc6619202278556a95e01f3c3ef36eb8
xavier gomez <axaviergp@gmail.com>
2025-01-06 11:50:35 -0600

    Creation of Modules

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman'
+  }
+
+  changeAge(): void {
+    this.age = 22
+  }
+
+  resetForm(): void {
+    this.age = 45;
+    this.name = 'ironman';
+  }
 
 }
